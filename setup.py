a60afe1750e065982b2f44839825d0c81cb38316
alexplevako <alex.plevako@scorum.com>
2018-10-18 17:13:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='mongo-exporter',
     version='0.0.1',
     packages=find_packages(exclude=["tests"]),
-    long_description=open('README.md').read(), install_requires=['pyyaml', 'pymongo', 'scorum'],
+    long_description=open('README.md').read(),
+    install_requires=['pyyaml', 'pymongo', 'scorum'],
     entry_points={
         'console_scripts': [
             'modex = modex.mongo:main',
