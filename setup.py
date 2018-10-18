9ccb27c6a3f69429c9ca1574f5a1917590ca732f
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-10-18 17:32:11 +0300

    scorum-py set to v0.0.1; set versions for dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+required = [
+    'pymongo',
+    'pyyaml',      
+    'scorum'
+]
+
 setup(
     name='mongo-exporter',
     version='0.0.1',
     packages=find_packages(exclude=["tests"]),
     long_description=open('README.md').read(),
-    install_requires=['pyyaml', 'pymongo', 'scorum'],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'modex = modex.mongo:main',
